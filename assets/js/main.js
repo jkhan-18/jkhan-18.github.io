69619ade4c59b3d3c752a66852c7ce22bb1758c0
jkhan18 <jkhan18@its.jnj.com>
2022-06-15 14:10:53 +0530

    Reverting Test commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -320,35 +320,4 @@
 
 		}
 
-		var myState = {
-      pdf: null,
-      currentPage: 1,
-      zoom: 1
-    }
-
-    pdfjsLib.getDocument("assets/pdf/Jahangir's Resume.pdf").then((pdf) => {
-
-      myState.pdf = pdf;
-      render();
-
-    });
-
-    function render() {
-      myState.pdf.getPage(myState.currentPage).then((page) => {
-
-        var canvas = document.getElementById("pdf_renderer");
-        var ctx = canvas.getContext('2d');
-
-        var viewport = page.getViewport(myState.zoom);
-
-        canvas.width = viewport.width;
-        canvas.height = viewport.height;
-
-        page.render({
-          canvasContext: ctx,
-          viewport: viewport
-        });
-      });
-    }
-
 })(jQuery);
